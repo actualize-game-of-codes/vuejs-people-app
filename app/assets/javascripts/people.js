663dbd86bbdf98b03644068df1f335389c4aebd0
Peter Jang <peter.x.jang@gmail.com>
2017-08-14 18:58:28 -0500

    Add button to delete a person

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -46,6 +46,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         this.people.push(newPerson);
         this.newPersonName = "";
         this.newPersonBio = "";
+      },
+      deletePerson: function(inputPerson) {
+        var index = this.people.indexOf(inputPerson);
+        this.people.splice(index, 1);
       }
     },
     computed: {
